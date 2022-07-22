f55644e44d3112813821920834d37d31b991e83b
kejiahp <popoolakejiah@gmail.com>
2022-07-22 15:28:02 +0100

    added locals

--- votingproject/settings.py
+++ votingproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from django.contrib import messages
+import django_heroku 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,6 @@ PAYSTACK_SECRET_KEY = os.environ.get("PAYSTACKSCRTKEY")
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 MEDIA_URL = '/media/'
+
+
+django_heroku.settings(locals())
