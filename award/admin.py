8b060e5032fa7d0974dbe69cd971365ef6ed9cce
kejiahp <popoolakejiah@gmail.com>
2022-07-22 15:59:10 +0100

    updated admin

--- award/admin.py
+++ award/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from award.models import AwardsCategory,AwardsContestant
+from award.models import AwardsCategory,AwardsContestant,AwardVotePurchase
 
 admin.site.register(AwardsContestant)
 admin.site.register(AwardsCategory)
+admin.site.register(AwardVotePurchase)
--- votingproject/settings.py
+++ votingproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY= os.environ.get("CLEVER_VOTING")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 
 ALLOWED_HOSTS = ['*']
 
