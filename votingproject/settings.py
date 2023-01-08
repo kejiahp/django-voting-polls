8ec8d3ff4d9eae44e8dda1682fd143c72d20ba2e
kejiahp <popoolakejiah@gmail.com>
2023-01-08 18:02:01 +0100

    add: allowed hosts

--- votingproject/settings.py
+++ votingproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY= os.environ.get("CLEVER_VOTING")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("CLEVER_VOTING_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",") 
 
 HASHID_SALT = os.environ.get('HASHID_SALT')
 
