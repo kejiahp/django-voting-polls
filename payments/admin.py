969f4c0affd370b44b3c1ee23ff57d4383d11578
kejiahp <popoolakejiah@gmail.com>
2023-02-05 17:34:32 +0100

    update: payments admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import WebhookTestModel, AwardVotingWebhookModel
 
 # Register your models here.
+admin.site.register(WebhookTestModel)
+admin.site.register(AwardVotingWebhookModel)
